24fe3aca197c22375cf1fa669916494dd816af3b
Mohammed AlShammari <mhmdfayedh@gmail.com>
2025-05-14 22:13:43 +0300

    Createing Spring Maven Project

---
+++ src/test/java/sa/qiwa/relationshipsJpa/RelationshipsJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package sa.qiwa.relationshipsJpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RelationshipsJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
