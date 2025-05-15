43331b9b885a3c13202ed8a597dc1639f9b09cd0
Mohammed AlShammari <mhmdfayedh@gmail.com>
2025-05-15 08:10:44 +0300

    add support for retrive and test delete and cascade

--- src/main/java/sa/qiwa/relationshipsJpa/dao/UserDAO.java
+++ src/main/java/sa/qiwa/relationshipsJpa/dao/UserDAO.java
@@ -3,5 +3,6 @@ package sa.qiwa.relationshipsJpa.dao;
 import sa.qiwa.relationshipsJpa.entity.User;
 
 public interface UserDAO {
-
+    User findUserById(int id);
+    void deleteUserById(int id);
 }
--- src/main/java/sa/qiwa/relationshipsJpa/dao/UserDAOImpl.java
+++ src/main/java/sa/qiwa/relationshipsJpa/dao/UserDAOImpl.java
@@ -1,9 +1,29 @@
 package sa.qiwa.relationshipsJpa.dao;
 
+import jakarta.persistence.EntityManager;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+import sa.qiwa.relationshipsJpa.entity.User;
+
 @Repository
 public class UserDAOImpl implements UserDAO{
+    private EntityManager entityManager;
 
+    @Autowired
+    public UserDAOImpl(EntityManager entityManager){
+        this.entityManager = entityManager;
+    }
 
+    @Override
+    public User findUserById(int id) {
+        return entityManager.find(User.class, id);
+    }
 
+    @Override
+    @Transactional
+    public void deleteUserById(int id) {
+        User user = entityManager.find(User.class, id);
+        entityManager.remove(user);
+    }
 }
