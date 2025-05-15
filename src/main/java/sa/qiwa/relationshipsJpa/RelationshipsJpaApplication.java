2e2cf15816559fabc5cc818fff685aebdd52bb1e
Mohammed AlShammari <mhmdfayedh@gmail.com>
2025-05-15 09:19:08 +0300

    add CLI to test saving author&books

--- src/main/java/sa/qiwa/relationshipsJpa/RelationshipsJpaApplication.java
+++ src/main/java/sa/qiwa/relationshipsJpa/RelationshipsJpaApplication.java
@@ -1,7 +1,12 @@
 package sa.qiwa.relationshipsJpa;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import sa.qiwa.relationshipsJpa.dao.AuthorDAO;
+import sa.qiwa.relationshipsJpa.entity.Author;
+import sa.qiwa.relationshipsJpa.entity.Book;
 
 @SpringBootApplication
 public class RelationshipsJpaApplication {
@@ -10,4 +15,35 @@ public class RelationshipsJpaApplication {
 		SpringApplication.run(RelationshipsJpaApplication.class, args);
 	}
 
+	@Bean
+	public CommandLineRunner commandLineRunner(AuthorDAO authorDAO) {
+
+		return runner -> {
+			saveAuthorAndBooks(authorDAO);
+		};
+	}
+
+	private void saveAuthorAndBooks(AuthorDAO authorDAO) {
+		/* Create Author and books objects */
+
+		Author author = new Author("Mohammed",
+				"AlShammari",
+				"m.alshammari@tst.com");
+
+		Book book1 = new Book("To Kill a Mockingbird by Harper Lee", "Desc............");
+		Book book2 = new Book("1984 by George Orwell", "Desc...........");
+		Book book3 = new Book("The Alchemist by Paulo Coelho", "Desc...........");
+
+		/*  Link books to its author */
+		author.add(book1);
+		author.add(book2);
+		author.add(book3);
+
+
+		/* Save Author and books */
+		System.out.println("Saving author and associated books");
+		authorDAO.save(author);
+
+
+	}
 }
